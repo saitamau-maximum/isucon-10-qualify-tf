9e4e49bd06b216374ba09b4d132a64656779cc4a
sor4chi <sorakawa0218@gmail.com>
2023-09-09 01:01:47 +0900

    netplah.shの複数行対応修正

--- script/netplan.sh
+++ script/netplan.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
-# fileに書いてあるipアドレスに対して、netplanを適用する
-# このスクリプトは、ip.txtというファイルにipアドレスを書いておく必要がある
-# また、ip.txtの中身は、ipアドレスと改行のみである必要がある
-# 例:
-#```ip.txt `
-#000.000.000.000
-#111.111.111.111
-#222.222.222.222
-#333.333.333.333
-#444.444.444.444
-#555.555.555.555
-#```
 
-num=2
-while read line
-do
-    echo $line
-    ssh -i ~/.ssh/isucon ubuntu@$line "sudo touch /etc/netplan/01-netcfg.yaml"
-    ssh -i ~/.ssh/isucon ubuntu@$line "sudo sh -c \"echo \"
+# load ip.txt
+TARGET_IPS=$(cat ip.txt)
+LOCAL_IP="192.168.0."
+COUNTER=2
+
+# loop through each ip
+for ip in $TARGET_IPS; do
+    # ssh into each ip and run the following command
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/isucon ubuntu@$ip "sudo rm /etc/netplan/01-netcfg.yaml && sudo touch /etc/netplan/01-netcfg.yaml && sudo chmod 666 /etc/netplan/01-netcfg.yaml"
+    TEMPLATE=$(cat <<EOF
+network:
+  version: 2
   ethernets:
     ens4:
-      addresses:
-        - 192.168.0.$num/24
       dhcp4: 'no'
       dhcp6: 'no'
-  renderer: networkd
-  version: 2
-\" > /etc/netplan/01-netcfg.yaml\" "
-    ssh -i ~/.ssh/isucon ubuntu@$line "sudo netplan apply"
-    echo "$line $num" >> log.txt
-    #numは2,3,4,5を循環する
-    if [ $num -eq 5 ]; then
-        num=2
-    else
-        num=$((num+1))
-    fi
-done < ip.txt
-
+      addresses:
+        - $LOCAL_IP$COUNTER/24
+EOF
+)
+    echo "$TEMPLATE" | ssh -o StrictHostKeyChecking=no -i ~/.ssh/isucon ubuntu@$ip "sudo tee -a /etc/netplan/01-netcfg.yaml"
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/isucon ubuntu@$ip "sudo netplan apply"
+    COUNTER=$((COUNTER+1))
+done
